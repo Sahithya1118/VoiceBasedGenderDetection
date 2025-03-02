358324bebc04b8dd005abd8d41fa0c1690269234
Sahithya1118 <sahithyaballa.2019654@gmail.com>
2025-03-02 17:48:02 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,5 +1,4 @@
 import os
-from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
 
 from utils import load_data, split_data, create_model
 
@@ -10,20 +9,14 @@ data = split_data(X, y, test_size=0.1, valid_size=0.1)
 # construct the model
 model = create_model()
 
-# use tensorboard to view metrics
-tensorboard = TensorBoard(log_dir="logs")
-# define early stopping to stop training after 5 epochs of not improving
-early_stopping = EarlyStopping(mode="min", patience=5, restore_best_weights=True)
-
-batch_size = 64
+batch_size = 65
 epochs = 100
 
 # train the model using the training set and validating using validation set
-model.fit(data["X_train"], data["y_train"], epochs=epochs, batch_size=batch_size, validation_data=(data["X_valid"], data["y_valid"]),
-          callbacks=[tensorboard, early_stopping])
+model.fit(data["X_train"], data["y_train"], epochs=epochs, batch_size=batch_size, validation_data=(data["X_valid"], data["y_valid"]))
 
 # save the model to a file
-model.save("results/model.h5")
+model.save("results/model91.h5")
 
 # evaluating the model using the testing set
 print(f"Evaluating the model using {len(data['X_test'])} samples...")
